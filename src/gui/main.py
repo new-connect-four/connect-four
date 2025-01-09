74513ec57eb573c3bbd6c34ae70d2e1ab8523b30
FullerBread2032 <sobczakd2017@gmail.com>
2025-01-09 16:10:02 +0100

    Added drop mechanic

--- src/gui/main.py
+++ src/gui/main.py
@@ -5,12 +5,9 @@ import pygame
 
 import src.gui.theme as theme
 
-# REMOVE
+# REMOVE | DEBUG
 board = [[0 for _ in range(COLUMNS)] for _ in range(ROWS)]
 
-board[1][1] = 1
-
-board[2][6] = 2
 # REMOVE
 
 def draw_token(surface, x, y, radius, color):
@@ -19,7 +16,24 @@ def draw_token(surface, x, y, radius, color):
     gfxdraw.aacircle(surface, x, y, radius - 4, color[0])
     gfxdraw.filled_circle(surface, x, y, radius - 4,color[0])
 
+def draw_board(screen):
+    size = 30
+    for j in range(COLUMNS):
+        for i in range(ROWS):
+            color = (theme.BACKGROUND, theme.BACKGROUND)
+            if board[i][j] == 1:
+                color = theme.YELLOW
+            elif board[i][j] == 2:
+                color = theme.RED
+            draw_token(screen, (10 + j * (size * 2 + 10) + size), (500 - size - 10) - (size * 2 + 10) * i, size, color)
+
 def main():
+
+    # REMOVE
+    global player1
+    player1 = True
+    # REMOVE
+
     game = Game()
     pygame.init()
     screen = pygame.display.set_mode([800, 500])
@@ -32,18 +46,27 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, theme.BACKGROUND, (0,0, 500, 500))
+                posx = event.pos[0]
+                draw_token(screen, min(max(posx, 40), 460), 35, 30, theme.YELLOW if player1 else theme.RED)
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                if 0 < pos[0] < 500:
+                    col = min((max(pos[0] - 5, 0)) // 70, 6)
+                    s = False
+                    for _ in range(ROWS):
+                        if board[_][col] == 0:
+                            board[_][col] = 1 if player1 else 2
+                            s = True
+                            break
+                    if not s:
+                        continue
+                    player1 = not player1
+                    draw_token(screen, min(max(posx, 40), 460), 35, 30, theme.YELLOW if player1 else theme.RED)
         pygame.draw.rect(screen, theme.BOARD, pygame.Rect(0, 70, 500, 500))
 
-        size = 30
-        for j in range(COLUMNS):
-            for i in range(ROWS):
-                color = (theme.BACKGROUND, theme.BACKGROUND)
-                if board[i][j] == 1:
-                    color = theme.YELLOW
-                elif board[i][j] == 2:
-                    color = theme.RED
-                draw_token(screen, (10 + j * (size * 2 + 10) + size), (500 - size - 10) - (size * 2 + 10) * i, size, color)
+        draw_board(screen)
 
         pygame.display.flip()
         pygame.time.Clock().tick(60)
