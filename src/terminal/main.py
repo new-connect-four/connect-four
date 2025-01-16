0e2a3973e1c4f36e33e3ded9864d927dda7e26d8
lambadel-ta <dankz0r085@gmail.com>
2025-01-16 17:51:13 +0100

    Ustawienia, generowanie pliku ustawien, typy ekranu, lepsza zmiana ekranu

--- src/terminal/main.py
+++ src/terminal/main.py
@@ -4,55 +4,120 @@ _default_settings = {
     'PLAYER1_DISC' : 'X',
     'PLAYER2_DISC' : 'O'
 }
-settings = {}
-with open("settings.txt","w+") as file:
-    for line in file:
-        x, _, y = line.split()
-        if x in _default_settings.keys():
-            settings[x] = y
+def initialize_settings():
+    settings = {}
+    with open("settings.txt", "r+") as file:
+        for line in file:
+            if "=" in line:
+                x, y = line.split("=", 1)
+                x = x.strip()
+                y = y.strip()
+                print(x)
+                if x in _default_settings:
+                    settings[x] = y
+                    print(settings)
+        for key, default_value in _default_settings.items():
+            if key not in settings:
+                settings[key] = default_value
+        file.seek(0)
+        file.truncate()
+        for key, value in settings.items():
+            file.write(f"{key} = {value}\n")
+        return settings
+settings = initialize_settings()
 
-def screens(name):
-    match name:
-        case "main_menu":
-            return [
-    ("####################","main_menu"),
-    ("#     CONNECT4     #","main_menu"),
-    ("####################\n","main_menu"),
-    ("Play","start_vsplayer"),
-    ("Settings","settings_scr"),
-    ("Exit","exit")
-            ]
-        case "settings_scr":
-            return [
-    ("####################", "settings_scr"),
-    ("#     SETTINGS     #", "settings_scr"),
-    ("####################\n", "settings_scr"),
-    ("Change Player 1 Disc: '" + str(settings.get("PLAYER1_DISC")) + "'",''),
-    ("Change Player 2 Disc: '" + str(settings.get("PLAYER2_DISC")) + "'", ''),
-    ("Return to Menu","main_menu")
-]
 
 
 class Screen:
+
     def __init__(self):
         self.current = "main_menu"
+        self.type = "menu"
+        self.setting = False
 
+    def screens(self,name):
+        match name:
+            case "main_menu":
+                return [
+                    "####################",
+                    "#     CONNECT4     #",
+                    "####################\n"
+                ], [
+                    ("[ Play ]", "menu", "start_vsplayer"),
+                    ("[ Settings ]", "menu", "settings_scr"),
+                    ("[ Exit ]", "exit", "exit")
+                ]
+            case "settings_scr":
+                return [
+                    "####################",
+                    "#     SETTINGS     #",
+                    "####################\n"
+                ], [
+                    (f"[ Player 1 Disc ] = '{settings["PLAYER1_DISC"]}'", "setting", "PLAYER1_DISC"),
+                    (f"[ Player 2 Disc ] = '{settings["PLAYER2_DISC"]}'\n", "setting", "PLAYER2_DISC"),
+                    ("[ Return to Menu ]", "menu", "main_menu")
+                ], "menu"
+            case "change_setting":
+                return f"Current Value: {settings[self.setting]}"
     def draw(self):
-        curr = screens(self.current)
-        for i in range(len(curr)):
-            print(i,curr[i][0])
-    def switch(self,n):
-        self.current = screens(self.current)[n][1]
+        curr = self.screens(self.current)
+        scrtype = self.type
+        match scrtype:
+            case "menu":
+                header = curr[0]
+                options = curr[1]
+                for i in header:
+                    print(i)
+                for i in range(len(options)):
+                    print(i, options[i][0], sep=' - ')
+            case "change_setting":
+                print(curr)
+    def switch(self,screen,scrtype):
+        self.type = scrtype
+        if scrtype == "setting":
+            self.setting = screen
+            self.current = "change_setting"
+        else:
+            self.current = screen
+            self.setting = False
         self.draw()
+
+def update_settingfile():
+    with open("settings.txt", "w+") as file:
+        file.seek(0)
+        file.truncate()
+        for key, value in settings.items():
+            file.write(f"{key} = {value}\n")
+
+def change_setting(setting,newvalue):
+    settings[setting] = newvalue
+    update_settingfile()
+
 def main():
     game = Game()
     screen = Screen()
     screen.draw()
-
+    #update_settingfile()
     while True:
-        x = input('')
-        print(screen.current)
-        screen.switch(int(x))
+        scrtype = screen.type
+        curr = screen.screens(screen.current)
+        match scrtype:
+            case "menu":
+                x = ''
+                while True:
+                    x = input('')
+                    if x.isdigit():
+                        x = int(x)
+                        screen.switch(curr[1][x][2],curr[1][x][1])
+                        break
+                    else:
+                        screen.draw()
+                        print("\nInvalid input!")
+            case "setting":
+                setting = screen.setting
+                x = input('Input new value: ')
+                change_setting(setting,x)
+                screen.switch("settings_scr","menu")
 
 if __name__ == "__main__":
     main()
